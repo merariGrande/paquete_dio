261f405b5c5f7cd86d64fb9e96dcfffea45f53a8
merariGrande <idalia.grande22@itca.edu.sv>
2023-10-26 22:14:36 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/img/paquete_dio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.img.paquete_dio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:paquete_dio/paquetedio.dart';
+
+void main() {
+  runApp(MyApp());
+}
