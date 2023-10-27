2f9136a275b0764c00cc9ba0e91399152331d7d6
merariGrande <idalia.grande22@itca.edu.sv>
2023-10-26 22:24:04 -0600

    agregando arran

--- lib/paquetedio.dart
+++ lib/paquetedio.dart
@@ -4,3 +4,17 @@ import 'package:dio/dio.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
